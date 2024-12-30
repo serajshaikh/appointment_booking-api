461b48650cf75c6e13d413e1d2282120ed662ed7
seraj <serazshaikh07@gmail.com>
2024-12-31 00:22:24 +0530

    updated

--- routes/booking.js
+++ routes/booking.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import getSlots from "../controllers/getSlots.js"; // Ensure the .js extension
-import bookSlot from "../controllers/bookSlot.js"; // Ensure the .js extension
+import getSlots from "../controllers/getSlots.js";
+import bookSlot from "../controllers/bookSlot.js";
 
 const router = Router();
 
